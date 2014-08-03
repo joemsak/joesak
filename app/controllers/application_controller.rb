2e713b947a106668a35c33c27c87f1db53d789e6
Joe Sak <joe@joesak.com>
2014-08-03 17:36:14 -0600

    Add VERY rudimentary 'current_profile' helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  helper_method :current_profile
+
   protect_from_forgery with: :exception
+
+  private
+  def current_profile
+    @profile ||= Profile.last
+  end
 end
