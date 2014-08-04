3e658d6fac2053c217d62b8d1eaf1c3da439c52f
Joe Sak <joe@joesak.com>
2014-08-04 11:05:11 -0400

    Move FormPageStruct stuff from NewGistPage to FormPageStruct

--- spec/support/form_page_struct.rb
+++ spec/support/form_page_struct.rb
@@ -1,2 +1,17 @@
 class FormPageStruct < PageStruct.new(:noop)
+  def submit_form
+    click_button('Save')
+  end
+
+  def has_validation_error?(name, type)
+    error_msg = error_message(name, type)
+    has_content?(error_introduction)
+    has_content?(error_msg)
+  end
+
+  private
+  def error_message(name, key)
+    attribute = attribute_name(name)
+    t("activerecord.errors.models.#{model_name.underscore}.attributes.#{attribute}.#{key}")
+  end
 end
--- spec/support/pages/new_gist_page.rb
+++ spec/support/pages/new_gist_page.rb
@@ -7,24 +7,9 @@ class NewGistPage < FormPageStruct
     fill_in(:gist_remote_id, with: input)
   end
 
-  def submit_form
-    click_button('Save')
-  end
-
-  def has_validation_error?(name, type)
-    error_msg = error_message(name, type)
-    has_content?(error_introduction)
-    has_content?(error_msg)
-  end
-
   private
   def model_name
     'Gist'
-  end
-
-  def error_message(name, key)
-    attribute = attribute_name(name)
-    t("activerecord.errors.models.gist.attributes.#{attribute}.#{key}")
   end
 
   def attribute_name(attr)
