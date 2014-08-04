e1f0ccfc7f3257a93413325745f704b5448323a5
Joe Sak <joe@joesak.com>
2014-08-04 11:09:00 -0400

    Remove specifics from AsHelper

--- spec/support/as_helper.rb
+++ spec/support/as_helper.rb
@@ -1,8 +1,9 @@
 module AsHelper
   def method_missing(name, *args)
     if match = name.to_s.match(/^as_(.+)$/)
-      developer = create(match[1].to_s, username: 'developer')
-      yield(developer)
+      type = match[1]
+      user = create(type.to_sym, username: type)
+      yield(user)
     else
       super
     end
