d33acab8030d74825208a5928e5d6b53a4a37ccb
Joe Sak <joe@joesak.com>
2014-08-18 22:52:20 -0400

    Add required info for profile seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-if Profile.find_by_username('joemsak')
+if Profile.find_by(username: 'joemsak')
   $stdout << "Profile 'joemsak' exists.\n"
 else
-  if profile = Profile.create(username: 'joemsak', name: 'Joe Sak')
+  if profile = Profile.create!(username: 'joemsak',
+                               name: 'Joe Sak',
+                               password: 'secret',
+                               password_confirmation: 'secret')
     $stdout << "Profile 'joemsak' created.\n"
   else
     $stdout << "Could not create a profile for 'joemsak'"
