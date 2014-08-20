96f13078792f4f4c89d9f2e8ba06b7e0806ddf9b
Joe Sak <joe@joesak.com>
2014-08-19 20:51:09 -0400

    Add flash messages for signing in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,10 @@ class SessionsController < ApplicationController
   def create
     if profile = Profile.authenticate(params[:username], params[:password])
       session[:profile_id] = profile.id
+      flash[:notice] = t('sessions.sign_in.correct')
       redirect_to root_profile_path
     else
+      flash.now[:error] = t('sessions.sign_in.incorrect')
       render :new
     end
   end
