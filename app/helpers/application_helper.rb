1e8b038f845a58701c1353c87430b8f6c4172279
Joe Sak <joe@joesak.com>
2014-08-19 20:55:30 -0400

    Add header with helpful navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def sign_up_link
+    unless current_profile
+      link_to t('application.sign_up'), sign_up_path
+    end
+  end
+
+  def sign_in_link
+    unless current_profile
+      link_to t('application.sign_in'), sign_in_path
+    end
+  end
+
+  def sign_out_link
+    if current_profile
+      link_to t('application.sign_out'), sign_out_path
+    end
+  end
+
+  def your_profile_link
+    if current_profile
+      link_to current_profile.name, root_profile_path
+    end
+  end
 end
